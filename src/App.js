1fa9af08178c99a0dcba55066a03ddab26e27294
MSP001-coder <muditwwe@gmail.com>
2023-01-15 14:14:27 +0530

    apna-cart files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+
+let a=10;
+
+function App() {
+  return (
+    <>
+     <Navbar/>
+    </>
+  );
+}
+
+export default App;
